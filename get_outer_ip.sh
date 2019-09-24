9d898fdac80602d936566d7077ae719984d5fc65
wuwei070 <qingbai242@126.com>
2019-09-24 23:47:41 +0800

    add exit code

--- get_outer_ip.sh
+++ get_outer_ip.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 #查询linux服务器出口ip
-cd /tmp;rm -f /tmp/getip* ;wget -q  "http://members.3322.org/dyndns/getip"  ; cat getip* 
+$E_XCD=65
+cd /tmp || { echo "Cannot change to necessary directory." >&2 exit $E_XCD; }
+rm -f /tmp/getip* ;wget -q  "http://members.3322.org/dyndns/getip"  ; cat getip* 
 
